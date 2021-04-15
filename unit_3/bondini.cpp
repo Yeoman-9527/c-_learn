ef609f527459217cc95e165091e9b275c08fa48b
yeoman <512594092yeoman@gmail.com>
2021-04-15 11:44:24 +0800

    company

---
+++ unit_3/bondini.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    cout << "Ding!...Operation \"HyperHyper\" is now activated!\n";
+    cout << "Enter your agent code:________\b\b\b\b\b\b\b\b";
+    long code;
+    cin >> code;
+    cout << "Ding!...You entered " << code << ".../n";
+    cout << "Ding!...Code verified! Proceed with Plan Z3!\n";
+    return 0;
+}
---
+++ unit_3/chartype.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char ch;
+	cout << "Enter a character: " << endl;
+	cin >> ch;
+	cout << "Hola: ";
+	cout << "Thank for the " << ch << " character." << endl;
+	return 0;
+}
+
+
